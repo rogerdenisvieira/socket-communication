56142f5a6c5cd754dcf3c512e765b175f89bbd76
Roger Vieira <rogerdenis@gmail.com>
2015-10-03 17:31:03 -0300

    adicionadas as classes para comunicação

---
+++ DesktopClient/Main.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TesteComunicaçãoSocket
+{
+    public partial class Main : Form
+    {
+        private BindingList<Mensagem> messages;
+
+        public Main()
+        {
+            InitializeComponent();
+            this.messages = new BindingList<Mensagem>();            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.messages.Add(new Mensagem {
+                TextoMensagem = this.textBox1.Text,     
+                TimeStamp = System.DateTime.Now,
+            });
+
+           this.textBox2.Text += String.Format("{0} : {1}{2}",messages.Last().TimeStamp.ToString(), messages.Last().TextoMensagem,Environment.NewLine);
+           //MessageBox.Show(messages.Last().TextoMensagem);
+            //this.dataGridView1.Update();
+        }
+
+
+    }
+}
---
+++ DesktopClient/Mensagem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteComunicaçãoSocket
+{
+    class Mensagem
+    {
+        public DateTime TimeStamp { get; set; }
+        public String TextoMensagem { get; set; }
+    }
+}
